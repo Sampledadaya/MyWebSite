4f0e16e6a41e2faca66ba3335f79bef1c22839fd
sampledadaya <kiryuxa791bk@Mail.ru>
2024-10-17 23:17:43 +0400

    First version

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,56 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+from .forms import UserRegisterForm, ArticleForm
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-def hello_world(request):
-    return HttpResponse('Hello, world!')
+def index(request):
+    return render(request, 'blog/index.html')
 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Аккаунт {username} был успешно создан!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def article_create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('article_list')
+    else:
+        form = ArticleForm()
+    return render(request, 'blog/article_form.html', {'form': form})
+
+@login_required
+def article_edit(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('article_list')
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'blog/article_form.html', {'form': form})
+
+@login_required
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'blog/article_list.html', {'articles': articles})
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Ограничение доступа к методам API
